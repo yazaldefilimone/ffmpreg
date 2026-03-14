014abb14fcabe48859910d5c4eae7d44922ab461
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-03-14 03:32:31 +0200

    *

--- src/converter/runner.rs
+++ src/converter/runner.rs
@@ -1,9 +1,8 @@
 use crate::cli;
-use crate::core::graph::nodes::{DecoderNode, EncoderNode, ResamplerNode};
 use crate::core::graph::Graph;
+use crate::core::graph::nodes::{DecoderNode, EncoderNode, ResamplerNode};
 use crate::core::resampler::AudioResampler;
 use crate::core::resolver::{CodecResolver, ContainerResolver};
-use crate::core::track::TrackFormat;
 use crate::core::traits::Resampler;
 use crate::io::{Input, Output};
 use crate::message::Result;
@@ -15,64 +14,48 @@ pub fn runner(options: &cli::RunArgs) -> Result<()> {
 	let mut input = Input::from_resolver(&options.input, &containers)?;
 	let mut builder = Output::builder(&options.output, &containers)?;
 
-	let stream_options = options.stream_options()?;
 	let mut graph = Graph::new();
 
-	for option in &stream_options {
-		if let Some(codec) = option.codec.as_ref() {
-			builder.format_mut().apply_codec(codec)?;
-		}
+	for option in options.stream_options()? {
+		builder.format_codec(&option.codec)?;
 
 		for track in input.tracks.audio_selector(&option.selector)? {
 			let decoder = codecs.decoder_for(track)?;
 			let encoder = codecs.encoder_for(track, builder.format_mut())?;
 
 			let decoder_id = graph.add(DecoderNode::new(decoder));
+			let mut after_decode = decoder_id;
 
-			let resampler_node = build_resampler_node(track, &*encoder);
-			let after_decode = if let Some(resampler) = resampler_node {
-				let resample_id = graph.add(resampler);
-				graph.link(decoder_id, resample_id);
-				resample_id
-			} else {
-				decoder_id
+			let format_out = encoder.format();
+			let format_in = track.format;
+
+			if let (Some(format_in), Some(format_out)) = (format_in.audio(), format_out.audio()) {
+				let resampler = AudioResampler::new(*format_in, *format_out);
+				if resampler.needed() {
+					let resampler = ResamplerNode::new(Box::new(resampler));
+					let resample_id = graph.add(resampler);
+					graph.link(after_decode, resample_id);
+					after_decode = resample_id;
+				}
 			};
 
 			let encoder_id = graph.add(EncoderNode::new(encoder));
 			graph.link(after_decode, encoder_id);
 
-			graph.set_entry(track.id, decoder_id);
+			graph.set(track.id, decoder_id);
 		}
 	}
 
 	let mut output = builder.build()?;
 
 	while let Some(packet) = input.read_packet()? {
-		let packets = graph.process(packet)?;
+		let packets = graph.run(packet)?;
 		output.write_all(packets)?;
 	}
 
-	let flushed = graph.flush()?;
-	for packet in flushed {
+	for packet in graph.flush()? {
 		output.write_packet(packet)?;
 	}
 
 	output.finalize()
-}
-
-fn build_resampler_node(
-	track: &crate::core::Track,
-	encoder: &dyn crate::core::traits::Encoder,
-) -> Option<ResamplerNode> {
-	let input_format = track.audio_format()?;
-	let output_format = match encoder.input_format() {
-		TrackFormat::Audio(fmt) => fmt,
-		_ => return None,
-	};
-	let resampler = AudioResampler::new(*input_format, output_format);
-	if resampler.needed() {
-		Some(ResamplerNode::new(Box::new(resampler)))
-	} else {
-		None
-	}
 }
