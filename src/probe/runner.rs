c9ab61cfc4bbdf0642f183b835ebca19d94df6b1
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-03-14 03:32:31 +0200

    *

--- src/probe/runner.rs
+++ src/probe/runner.rs
@@ -1,23 +1,24 @@
+use crate::probe::Builder;
 use crate::probe::render::text::TextRender;
-use crate::probe::{Builder, render};
 use crate::{cli, io::Input, io::Output, message};
 
-pub fn runner(cmd: &cli::ProbeCommand) -> message::Result<()> {
+pub fn runner(cmd: &cli::ProbeArgs) -> message::Result<()> {
 	let input = Input::open(&cmd.input)?;
-	let output = cmd.output.clone().map(|output| Output::new(output)).transpose()?;
+	let output = cmd.output.as_ref().map(|output| Output::new(output)).transpose()?;
 
 	let builder = Builder::new(input, output);
 
 	let media = builder.media_file()?;
+	let mut renderer = TextRender::default();
+	let text = renderer.render(&media);
+	println!("{}", text);
 
-	let output = match cmd.json {
-		Some(cli::JsonOption::Pretty) => render::json::render_pretty(&media)?,
-		Some(cli::JsonOption::Raw) => render::json::render_raw(&media)?,
-		None => {
-			let mut renderer = TextRender::default();
-			renderer.render(&media)
-		}
-	};
-	println!("{}", output);
+	// let output = match cmd {
+	// 	Some(cli::JsonOption::Pretty) => render::json::render_pretty(&media)?,
+	// 	Some(cli::JsonOption::Raw) => render::json::render_raw(&media)?,
+	// 	None => {
+
+	// }
+	// };
 	Ok(())
 }
