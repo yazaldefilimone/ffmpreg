ab5a446652d9d65c8258f5b641737835a9a04905
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-03-14 03:32:30 +0200

    *

--- src/cli/runner.rs
+++ src/cli/runner.rs
@@ -1,10 +1,10 @@
 use crate::cli::args::*;
-// use crate::{converter, message::*, play, probe};
+use crate::{converter, message::*, play, probe};
 
-// pub fn runner(cli: Cli) -> Result<()> {
-// 	match cli.command {
-// 		Commands::Run(options) => converter::runner(&options),
-// 		Commands::Play(command) => play::runner(&command),
-// 		Commands::Probe(command) => probe::runner(&command),
-// 	}
-// }
+pub fn runner(cli: Cli) -> Result<()> {
+	match cli.command {
+		Commands::Run(options) => converter::runner(&options),
+		Commands::Play(command) => play::runner(&command),
+		Commands::Probe(command) => probe::runner(&command),
+	}
+}
