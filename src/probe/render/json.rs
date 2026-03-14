fcffaec37034204c58bf1e53ef2bc09986896300
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-03-14 03:32:31 +0200

    *

--- src/probe/render/json.rs
+++ src/probe/render/json.rs
@@ -2,10 +2,12 @@ use crate::error;
 use crate::message::Result;
 use crate::probe::model;
 
+#[allow(dead_code)]
 pub fn render_raw(media: &model::MediaFile) -> Result<String> {
 	serde_json::to_string(media).map_err(|err| error!("json render failed: {}", err))
 }
 
+#[allow(dead_code)]
 pub fn render_pretty(media: &model::MediaFile) -> Result<String> {
 	serde_json::to_string_pretty(media).map_err(|err| error!("json render failed: {}", err))
 }
