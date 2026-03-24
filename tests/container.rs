8e40b58613a2b57862e78a666cb1f448150af811
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-03-25 01:08:14 +0200

    test(wav): cover demuxer, muxer and fixture roundtrip

---
+++ tests/container.rs
@@ -0,0 +1,8 @@
+#[path = "container/wav/demuxer_test.rs"]
+mod demuxer_test;
+
+#[path = "container/wav/fixture_test.rs"]
+mod fixture_test;
+
+#[path = "container/wav/muxer_test.rs"]
+mod muxer_test;
