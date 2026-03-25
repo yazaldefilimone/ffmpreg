ef4137bc9faa308f3fd2779d69cfb4d73de27c97
Yazalde Filimone <yazaldefilimon@gmail.com>
2026-03-25 19:15:40 +0200

    test(container): move wav to riff module

--- tests/container.rs
+++ tests/container.rs
@@ -1,8 +1,8 @@
-#[path = "container/wav/demuxer_test.rs"]
+#[path = "container/riff/demuxer_test.rs"]
 mod demuxer_test;
 
-#[path = "container/wav/fixture_test.rs"]
+#[path = "container/riff/fixture_test.rs"]
 mod fixture_test;
 
-#[path = "container/wav/muxer_test.rs"]
+#[path = "container/riff/muxer_test.rs"]
 mod muxer_test;
